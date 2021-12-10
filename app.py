b4d24de217f636caed605b52b3e68038997b7e8d
Ajay Vardhan Reddy <47106551+Ajayvardhanreddy@users.noreply.github.com>
2021-12-10 23:04:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import os
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'learnpymongo'
-app.config['MONGO_URI'] = "mongodb+srv://ajayvardhanreddy:ajayvardhan@learnmongo.dba5m.mongodb.net/learnpymongo?retryWrites=true&w=majority"
+# app.config['MONGO_URI'] = "mongodb+srv://ajayvardhanreddy:ajayvardhan@learnmongo.dba5m.mongodb.net/learnpymongo?retryWrites=true&w=majority"
+app.config['MONGO_URI'] =os.environ.get("DATABASE_URL")
 
 mongo = PyMongo(app)
 
